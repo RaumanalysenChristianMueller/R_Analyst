798da10f29519523836b9f7eff27d5b8c6bf80e9
RaumanalysenChristianMueller <raumanalysen@mailbox.org>
2026-09-02 17:54:07 +0200

    Dockerfile angepasst

--- hole_punch_binder.R
+++ hole_punch_binder.R
@@ -3,6 +3,6 @@
 library(holepunch)
 write_compendium_description(package = "R-Analyst", 
                              description = "Test-Umgebung zur Fortbildung")
-write_dockerfile(maintainer = "Christian_Mueller-Kett") 
+write_dockerfile(maintainer = "ChristianMuellerKett", base = "rocker/binder:4.4.2") 
 generate_badge()
 # build_binder()
